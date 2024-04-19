910c89630e2273cfa5e1042ea0cb0fe07e24adde
Matias <matiasgsn@gmail.com>
2024-04-18 21:50:04 -0300

    Modo noturno funcional

---
+++ darkmode.js
@@ -0,0 +1,21 @@
+let btnMode = document.querySelector('#btnMode')
+let body = document.querySelector('body')
+let lightmodeimg = document.querySelector('#modeimg')
+let selected = 0
+
+btnMode.addEventListener('click', function(modeswap){
+    console.log("Modo alterado")
+    if (selected == 0) {
+        selected = 1 
+        document.body.setAttribute('data-bs-theme', 'dark')
+        modeimg.setAttribute('src', 'lightmode.svg')
+    } else {
+        selected = 0
+        document.body.setAttribute('data-bs-theme', 'light')
+        modeimg.setAttribute('src', 'darkmode.svg')
+    }
+    
+
+
+
+})
