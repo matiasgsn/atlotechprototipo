2d3487e7e0905b8ed9727a5fee82b1070119c09f
Matias <matiasgsn@gmail.com>
2024-04-19 13:22:53 -0300

    Otimização

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 let btnMode = document.querySelector('#btnMode') // Botão de selecionar modo
 let modeimg = document.querySelector('#modeimg') // Imagem do seletor de modo
 let body = document.querySelector('body')
-let logo = document.querySelectorAll('#logo')
 
 let selected = 0
+
+
+// Estilos de Cor
+
+let logoAtlo = document.querySelectorAll('#logo') // Logo da Atlo
+let iconWhatsapp = document.querySelector('#whatsapp') // Ícone do Whatsapp
+function styleWhiteAdd() { // Define os ícones a receberem o filtro .white
+    // Logos da Atlo
+    logoAtlo[0].classList.add('white')
+    logoAtlo[1].classList.add('white')
+
+    // Whatsapp
+    iconWhatsapp.classList.remove('atlo-primary')
+    iconWhatsapp.classList.add('white')
+}
+
+function styleWhiteRemove() { // Define os ícones a perderem o filtro .white
+    // Logos da Atlo
+    logoAtlo[0].classList.remove('white')
+    logoAtlo[1].classList.remove('white')
+
+    // Whatsapp
+    iconWhatsapp.classList.remove('white')
+    iconWhatsapp.classList.add('atlo-primary')
+}
 
 btnMode.addEventListener('click', function(modeswap){
     console.log("Modo alterado")
     if (selected == 0) {
         selected = 1 
         document.body.setAttribute('data-bs-theme', 'dark')
-        logo[0].classList.add('white')
-        logo[1].classList.add('white')
+        styleWhiteAdd()
         modeimg.setAttribute('src', 'lightmode.svg')
         
     } else {
         selected = 0
         document.body.setAttribute('data-bs-theme', 'light')
-        logo[0].classList.remove('white')
-        logo[1].classList.remove('white')
+        styleWhiteRemove()
         modeimg.setAttribute('src', 'darkmode.svg')
     }
 })
